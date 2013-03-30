c9893408b968806f7a9138a0e1eee41fabdc4c5c
martincronje <martin@nreality.com>
2013-03-30 16:17:03 +0200

    Added Hello World on iOS and Android in ObjC and Java

--- src/objc/HelloWorld/HelloWorld/NRViewController.m
+++ src/objc/HelloWorld/HelloWorld/NRViewController.m
@@ -18,6 +18,16 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 100, 20)];
+    
+    label.backgroundColor = [UIColor clearColor];
+    label.textColor = [UIColor redColor];
+    
+    label.text = @"Hello Teched";
+    label.font = [UIFont systemFontOfSize:13.0f];
+    
+    [self.view addSubview:label];
 }
 
 - (void)didReceiveMemoryWarning
