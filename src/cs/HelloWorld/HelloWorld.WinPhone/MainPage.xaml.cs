fc4e619455393cb975d9e7479874efb999ee5812
martincronje <martin@nreality.com>
2013-04-08 23:27:08 +0200

    Cherry picked 39178786a1a9207e1be756df5eaa57bfd6bd3d9c

--- src/cs/HelloWorld/HelloWorld.WinPhone/MainPage.xaml.cs
+++ src/cs/HelloWorld/HelloWorld.WinPhone/MainPage.xaml.cs
@@ -12,12 +12,12 @@ namespace HelloWorld.WinPhone
             InitializeComponent();
 
             var textBlock = new TextBlock
-                {
-                    Text = "Hello TechEd 2013",
-                    FontSize = 30,
-                    FontFamily = new FontFamily("Arial"),
-                    FontWeight = FontWeights.Bold
-                };
+            {
+                Text = "Hello TechEd 2013",
+                FontSize = 30,
+                FontFamily = new FontFamily("Arial"),
+                FontWeight = FontWeights.Bold
+            };
 
             ContentPanel.Children.Add(textBlock);
         }
